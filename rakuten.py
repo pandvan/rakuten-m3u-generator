9356f74c0d7b810c6066da9c11746a69e479c094
anycodesdev <103193786+anycodesdev@users.noreply.github.com>
2024-09-06 03:24:53 +0100

    Update rakuten.py

--- rakuten.py
+++ rakuten.py
@@ -35,7 +35,7 @@ class Api:
     referer = "https://rakuten.tv/"
     user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"
 
-    language = "it"
+    language = "fr"
 
 
     @classmethod
